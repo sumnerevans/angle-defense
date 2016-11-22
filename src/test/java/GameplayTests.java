9ec4c5107aff50263fb4925b573008d50092be35
Robinson Merillat <rdmerillat@mymail.mines.edu>
2016-11-22 10:45:44 -0700

    almost done with a couple more tests

--- src/test/java/GameplayTests.java
+++ src/test/java/GameplayTests.java
@@ -1,8 +1,12 @@
 import angleDefenseLogic.*;
+import angleDefenseLogic.Minions.AirUnit;
 import angleDefenseLogic.Minions.GroundUnit;
+import angleDefenseLogic.Towers.AirGroundTower;
+import angleDefenseLogic.Towers.AirTower;
 import angleDefenseLogic.Towers.GroundTower;
 import org.junit.*;
 
+import java.awt.*;
 import java.io.FileNotFoundException;
 import java.util.*;
 
@@ -65,17 +69,36 @@ public class GameplayTests {
     // Test Minion advance
     @Test
     public void testMinionAdvance() {
+        Minion one = new GroundUnit();
+        Minion two = new AirUnit();
+
+
         // TODO: Ensure that the minions advance at the correct speeds
 
         // TODO: Ensure that the minions stay on the paths as they advance.
+
     }
 
     // Test tower fire angle
     @Test
     public void testTowerFileAngle() {
+        Board board = new Board(64,48);
         // TODO: DO THESE TESTS FOR ALL TOWER TYPES
         // TODO: Place a tower and set its angle
+        Tower bang = new AirGroundTower();
+        Tower pew = new AirTower();
+        Tower kaboom = new GroundTower();
+
+        bang.setAngle(0);
+        pew.setAngle(0);
+        kaboom.setAngle(0);
+
+        bang.setPosition(24, 13);
+        pew.setPosition(24,12);
+        kaboom.setPosition(24,11);
+
         // TODO: Place a minion and place it on a spot where the tower can hit it
+        Minion ded = new GroundUnit();
         // TODO: Have the tower attack the minion and ensure that the minion dies
 
         // TODO: Place a tower and set its angle
@@ -88,6 +111,17 @@ public class GameplayTests {
     public void testTowerAttackMechanisms() {
         // TODO: DO THESE TESTS FOR ALL TOWER TYPES
         // TODO: Place one of each type of tower
+        Tower bang = new AirGroundTower();
+        Tower pew = new AirTower();
+        Tower kaboom = new GroundTower();
+
+        bang.setAngle(0);
+        pew.setAngle(0);
+        kaboom.setAngle(0);
+
+        bang.setPosition(24, 13);
+        pew.setPosition(24,12);
+        kaboom.setPosition(24,11);
         // TODO: Place a few minions of the correct type in the fire area of the tower.
         // TODO: Ensure that the minions die
 
@@ -98,9 +132,15 @@ public class GameplayTests {
     // Test player gold reward
     @Test
     public void testPlayerGoldReward() {
+        Player test = new Player("test", Color.BLUE);
         // TODO: Place a tower
+        Tower pew = new AirTower();
+        // TODO: Place a minion
+        Minion eek = new AirUnit();
         // TODO: Kill a minion
+        eek.attacked(pew, Integer.MAX_VALUE);
         // TODO: Ensure that the player's gold is incremented by the value of the minion
+        assertEquals(test.getGold(),3); // TODO: change the 3 to whatever gold amount it should be
     }
 
 }
