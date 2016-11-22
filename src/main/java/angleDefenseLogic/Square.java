920e518861c56788c76d4cfb68d71c0b43041230
Sumner Evans <sumner.evans98@gmail.com>
2016-11-22 11:23:33 -0700

    Added tests for squares

--- src/main/java/angleDefenseLogic/Square.java
+++ src/main/java/angleDefenseLogic/Square.java
@@ -6,24 +6,31 @@ import java.util.*;
 
 public class Square implements IDrawable {
 
+
     public enum SquareType {
-        GROUND,
-        TRENCH
+        GROUND, TRENCH
     }
 
     public enum CliffSide {
         TOP, BOTTOM, LEFT, RIGHT
     }
 
+    public enum Decoration {
+        FLAG, PILLAR
+    }
+
     private int x, y;
     private SquareType squareType;
     private List<CliffSide> cliffSides;
+    private List<Decoration> decorations;
 
-    public Square(int x, int y, SquareType squareType, List<CliffSide> cliffSides) {
+    public Square(int x, int y, SquareType squareType, List<CliffSide> cliffSides,
+                  List<Decoration> decorations) {
         this.x = x;
         this.y = y;
         this.squareType = squareType;
         this.cliffSides = cliffSides;
+        this.decorations = decorations;
     }
 
     @Override
@@ -40,4 +47,7 @@ public class Square implements IDrawable {
         return this.cliffSides;
     }
 
+    public List<Decoration> getDecorations() {
+        return this.decorations;
+    }
 }
