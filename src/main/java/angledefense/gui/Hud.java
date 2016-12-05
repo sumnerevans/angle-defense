aae59b28bddb9b126ff77f10beead1d530363962
Robinson Merillat <rdmerillat@mymail.mines.edu>
2016-12-05 12:23:14 -0700

    GUI display screen

--- src/main/java/angledefense/gui/Hud.java
+++ src/main/java/angledefense/gui/Hud.java
@@ -6,24 +6,32 @@ import java.util.*;
 /**
  * Created by Sumner on 11/21/16.
  */
-public class Hud {
+public class Hud extends JFrame {
     private JPanel board;
     private ArrayList<JButton> buttons;
     private ArrayList<JLabel> textLabels;
 
     public Hud() {
-        board = new JPanel();
-        buttons = new ArrayList<>();
-        textLabels = new ArrayList<>();
+        initializeGUI();
 
+    }
 
+    private void initializeGUI() {
+        setSize(900, 600);
+        setTitle("Angle Defence");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 
     public void drawContent() {
         // TODO: Implement
     }
 
-    public void createButton() {
+    private JButton createButton(){
+        JButton button = new JButton();
+        return button;
+    }
+
+    public void createButtons() {
         // TODO: make this take in an image and return a button object
         // TODO: use Game.buildTower(Tower t) to build towers
     }
@@ -31,5 +39,6 @@ public class Hud {
     // To be used only for viewing while creating the GUI
     public static void main(String[] args) {
         Hud hud = new Hud();
+        hud.setVisible(true);
     }
 }
